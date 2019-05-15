3bad1f816293ca0deaedcd5b25f78d4b0762af0f
juanlu-millan <juanlumillanhidalgo@gmail.com>
2019-05-15 11:10:07 +0200

    Heroku

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template,abort
 app = Flask(__name__)
 
+port=os.environ["PORT"]
+
 @app.route('/')
 def inicio():
 	return render_template("index.html")
 
-app.run(debug=True)
+@app.route('/busqueda')
+def busqueda():
+	return render_template("busqueda.html")
+
+
+app.run('0.0.0.0',int(port), debug=True)
