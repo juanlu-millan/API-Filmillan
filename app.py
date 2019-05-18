ffcebfeb9ad67a9a8b77ddb9fae3efed5338ed4b
juanlu-millan <juanlumillanhidalgo@gmail.com>
2019-05-18 12:29:35 +0200

    Ultimos Lanzamientos

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template,abort
-import os
+from flask import Flask, render_template,abort,request
+import os,requests
 app = Flask(__name__)
 
-port=os.environ["PORT"]
+URL_BASE_TMDB = 'https://api.themoviedb.org/3/'
 
-@app.route('/')
+# port=os.environ["PORT"]
+language = 'es-ES'
+
+@app.route('/',methods = ['GET'])
 def inicio():
-	return render_template("index.html")
+    pay = {'api_key': '53bcf930f2611a01d6a893b431703e79','language': language,'page' : '1'}
+    ultimos = requests.get(URL_BASE_TMDB + 'movie/now_playing',  params=pay)
+    if ultimos.status_code == 200:
+	      cart = ultimos.json()
+	      lista = []
+	      for i in cart['results']:
+	           lista.append({'titulo':i['title'],'poster': i['poster_path']})
+	      return render_template('index.html', lista = lista)
 
 @app.route('/busqueda')
 def busqueda():
-	return render_template("busqueda.html")
+    return render_template("busqueda.html",)
 
 
-app.run('0.0.0.0',int(port), debug=True)
+# app.run('0.0.0.0',int(port), debug=True)
+app.run(debug=True)
