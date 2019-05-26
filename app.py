933cddcbf91c371e5d214a54ad5a6ea008be3d78
juanlu-millan <juanlumillanhidalgo@gmail.com>
2019-05-27 00:30:32 +0200

    info de peli

--- app.py
+++ app.py
@@ -8,17 +8,21 @@ port = os.environ['PORT']
 
 language = 'es-ES'
 key = os.environ['key']
+noimg = None
+
 
 @app.route('/',methods = ['GET'])
 def inicio():
     pay = {'api_key': key,'language': language,'page' : '1'}
     ultimos = requests.get(URL_BASE_TMDB + 'movie/now_playing',  params=pay)
+
     if ultimos.status_code == 200:
-	      cart = ultimos.json()
-	      lista = []
-	      for i in cart['results']:
-	           lista.append({'titulo':i['title'],'poster': i['poster_path']})
-	      return render_template('index.html', lista = lista)
+        cart = ultimos.json()
+        lista = []
+        for i in cart['results']:
+            lista.append({'titulo':i['title'],'poster': i['poster_path'],'id':i['id']})
+            # info(int(i['id']),"pelicula")
+        return render_template('index.html', lista = lista)
 
 @app.route('/busqueda', methods = ['GET', 'POST'])
 def busqueda():
@@ -27,46 +31,71 @@ def busqueda():
         else:
             tituloform = request.form['busqueda']
             if tituloform != '':
-                if request.form['tipo'] == 'pelis':
+                if request.form['selec'] == 'pelicula':
                     payload = {'api_key': key,'language': language,'page' : '1','query': tituloform}
                     ultimos = requests.get(URL_BASE_TMDB + 'search/movie',  params = payload)
                     if ultimos.status_code == 200:
                         search = ultimos.json()
                         lista = []
                         listanoimg = []
-                        noimg = None
                         for resultado in search['results']:
                             if resultado['poster_path'] != noimg:
                                 lista.append({'titulo':resultado['title'],'id':resultado['id'],'poster': resultado['poster_path']})
                             else:
                                 listanoimg.append({'titulo':resultado['title'],'id':resultado['id']})
 
-                        return render_template("busqueda.html", lista = lista, listanoimg = listanoimg, busqueda = tituloform)
+                        return render_template("busqueda.html", lista = lista, seleccion = request.form['selec'] , listanoimg = listanoimg, busqueda = tituloform)
                 else:
-                    payload = {'api_key': 'key','language': language,'page' : '1','query': tituloform}
+                    payload = {'api_key': key,'language': language,'page' : '1','query': tituloform}
                     ultimos = requests.get(URL_BASE_TMDB + 'search/tv',  params = payload)
                     if ultimos.status_code == 200:
                         search = ultimos.json()
                         lista = []
                         listanoimg = []
-                        noimg = None
                         for resultado in search['results']:
                             if resultado['poster_path'] != noimg:
                                 lista.append({'titulo':resultado['name'],'id':resultado['id'],'poster': resultado['poster_path']})
                             else:
                                 listanoimg.append({'titulo':resultado['name'],'id':resultado['id']})
-                        return render_template("busqueda.html", lista = lista,listanoimg = listanoimg, busqueda = tituloform)
+                        return render_template("busqueda.html", lista = lista,listanoimg = listanoimg, seleccion = request.form['selec'] , busqueda = tituloform)
 
-@app.route('/pelicula/<id>')
-def info(id):
+@app.route('/<selec>/<id>')
+def info(id,selec):
     error = None
     payload = {'api_key': key,'language': language}
     info = requests.get(URL_BASE_TMDB + 'movie/' + id,  params = payload)
-    if info.status_code == 200:
-        infopeli = info.json()
-        pelisinfo = {'titulo':infopeli['title'],'poster':infopeli['poster_path'],'sinopsis':infopeli['overview'],'year':infopeli['release_date'],'notatmdb':infopeli['vote_average']}
-        return render_template("id.html" , lista = pelisinfo,  error = error)
+    if selec == "pelicula":
+        if info.status_code == 200:
+            infopeli = info.json()
+            pelisinfo = {'titulo':infopeli['title'],'poster':infopeli['poster_path'],'sinopsis':infopeli['overview'],'year':infopeli['release_date'],'notatmdb':infopeli['vote_average']}
+            payact = {'api_key': key}
+            acto = requests.get(URL_BASE_TMDB + 'movie/' + id +'/credits', params = payact)
+            if acto.status_code == 200:
+                actores = acto.json()
+                reparto = actores['cast']
+                infoactor = []
+                for actor in reparto:
+                    if actor['profile_path'] != noimg:
+                        infoactor.append({'actores':actor["name"],'fotos':actor['profile_path']})
+                infoactor = infoactor[0:8]
+                return render_template("id.html" , lista = pelisinfo, reparto = infoactor ,  error = error)
 
+    else:
+        info = requests.get(URL_BASE_TMDB + 'tv/' + id,  params = payload)
+        if info.status_code == 200:
+            infoserie = info.json()
+            seriesinfo = {'titulo':infoserie['name'],'poster':infoserie['poster_path'],'sinopsis':infoserie['overview'],'year':infoserie['first_air_date'],'notatmdb':infoserie['vote_average']}
+            payact = {'api_key': key}
+            acto = requests.get(URL_BASE_TMDB + 'tv/' + id +'/credits', params = payact)
+            if acto.status_code == 200:
+                actores = acto.json()
+                reparto = actores['cast']
+                infoactor = []
+                for actor in reparto:
+                    if actor['profile_path'] != noimg:
+                        infoactor.append({'actores':actor["name"],'fotos':actor['profile_path']})
+                infoactor = infoactor[0:8]
+                return render_template("id.html" , lista = seriesinfo, reparto = infoactor ,  error = error)
 
 
 
